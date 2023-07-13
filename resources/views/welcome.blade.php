c5fe5533fe485bebdaff1032796f307ae26d35d6
IsaacVega <isaacvega361@gmail.com>
2023-07-13 17:21:43 -0400

    tri fix css|js 01

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="{{ asset('css/style.css ') }}">
 </head>
 
 <body>
@@ -50,7 +50,7 @@
     </script>
 
     <!-- Script -->
-    <script src="./js/script.js"></script>
+    <script src="{{ asset('js/script.js ') }}"></script>
 </body>
 
 </html>
